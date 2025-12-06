4f45cc40c5eefc7f5da974a392465d86cc9190b4
Maged Gamail <magedgamail2391@gmail.com>
2025-12-06 19:57:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -24,6 +24,9 @@ const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.get('/', (req, res) => {
+    res.json({ message: "Welcome to the Ecommerce API. Check /api/products for data." });
+});
 
 app.use(cors());
 app.use(express.json());
